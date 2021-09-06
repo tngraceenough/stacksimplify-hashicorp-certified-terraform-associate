505c685175c705d08a349b509861c2c944556a94
tngraceenough <78455035+tngraceenough@users.noreply.github.com>
2021-09-05 22:09:09 -0400

    Update and rename c2-versions.tf to c1-versions.tf

--- 06-Terraform-Datasources/terraform-manifests/c1-versions.tf
+++ 06-Terraform-Datasources/terraform-manifests/c1-versions.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = "~> 1.xx" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx  required_providers {
+  required_version = "~> 1.04"which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
